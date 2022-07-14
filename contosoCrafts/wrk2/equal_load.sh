7ec0464abd5ebdbfeb942b359069015bcb2782a3
wolski0420 <wolski0420@gmail.com>
2022-07-14 16:48:39 +0200

    Providing new script for automating vtune profiler for given microservices

--- contosoCrafts/wrk2/equal_load.sh
+++ contosoCrafts/wrk2/equal_load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for i in {1..50..1}
+for i in {1..100..1}
 do
   ./wrk -t 8 -c 8 -d 15 -L -s ./scripts/contosoCrafts/get_all.lua http://localhost:9090/Products -R 2000
 done
--- reactive-company/wrk2/equal_load.sh
+++ reactive-company/wrk2/equal_load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for i in {1..50..1}
+for i in {1..100..1}
 do
   ./wrk -t 8 -c 8 -d 15 -L -s ./scripts/reactive-company/get_all_blogposts_stream.lua http://localhost:8080/stream -R 2000
 done
